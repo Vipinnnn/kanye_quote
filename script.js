47d08b1c2da032d990d3033ccd18b3a42d4be808
Vipinnnn <2019vipin.talreja@ves.ac.in>
2023-08-27 14:57:56 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const joke = document.getElementById("joke");
+const jokeBtn = document.getElementById("joke-btn");
+
+const generateJoke = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://api.kanye.rest");
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        joke.innerHTML = JSON.parse(this.responseText).quote;
+      } else {
+      }
+    }
+  };
+  xhr.send();
+};
+
+jokeBtn.addEventListener("click", generateJoke);
